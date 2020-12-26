1cd36cc06d18fcd748c1ff98f38898866c5d016d
Didier <didier.abder@gmail.com>
2020-12-26 19:44:54 +0100

    -

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM tomcat:9.0
+
+COPY ./target/spring5-jee8-minimal-1.0-SNAPSHOT.war /usr/local/tomcat/webapps/
--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-mvnw cargo:run
+docker run -it -p 8080:8080 -v %cd%/target/spring5-jee8-minimal-1.0-SNAPSHOT.war:/usr/local/tomcat/webapps/minimal.war tomcat:9.0
